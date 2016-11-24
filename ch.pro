bbc0e7ac1ccb191f95ad73f726f431bac3d321de
Dmytro Prokofiev <prokofjev.d@gmail.com>
2016-11-24 12:01:00 +0200

    transfer exporter.h logic to maon.cpp

--- ch.pro
+++ ch.pro
@@ -9,7 +9,6 @@ QT += qml quick \
 CONFIG+=c++14
 
 HEADERS += cpp/board_graphic.h \
-           cpp/exporter.h \
            cpp/enums.h \
            cpp/my_socket.h \
            cpp/fb_obj.h \
@@ -19,7 +18,6 @@ HEADERS += cpp/board_graphic.h \
 
 SOURCES += cpp/main.cpp\
            cpp/board_graphic.cpp \
-           cpp/exporter.cpp \
            cpp/my_socket.cpp \
            cpp/fb_obj.cpp \
            cpp/cube_renderer.cpp
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -6,7 +6,6 @@
 #include <exception>
 
 #include "board_graphic.h"
-#include "exporter.h"
 #include "my_socket.h"
 #include "fb_obj.h"
 
@@ -23,8 +22,6 @@ int main(int argc, char *argv[])
   engine.load(QUrl(QStringLiteral("../c/res/app.qml")));
 
   inet::my_socket socket;
-  Exporter exporter(board_graphic, socket);
-
   const double CHECK_TIME = 0.015 * CLOCKS_PER_SEC;
   clock_t timer = clock();
 
@@ -36,11 +33,45 @@ int main(int argc, char *argv[])
     {
       timer = clock();
 
-      while(board_graphic.is_new_message_for_server_appear())
-        { exporter.push_to_socet(board_graphic.pull_first_messages_for_server()); }
+      while(board_graphic.is_message_appear())
+      {
+        QByteArray m;
+        m.append(board_graphic.pull());
+        socket.send(m);
+      }
 
       while(socket.is_message_append())
-        { exporter.push_to_graphic(socket.pull()); }
+      {
+         const QString message = socket.pull();
+         const int type = message.mid(0, message.indexOf(" ")).toInt();
+         if(type == Messages::INF_REQUEST)
+           { board_graphic.add_to_command_history(message.mid(message.indexOf(" "))); }
+
+         else if(type == Messages::SERVER_LOST
+                 || type == Messages::SERVER_HERE
+                 || type == Messages::OPPONENT_LOST)
+           { board_graphic.set_connect_status(message.toInt()); }
+
+         else if(type == Messages::GAME_INF)
+         {
+           QString m = message.mid(message.indexOf(" ") + 1);
+           const int INDEX = m.indexOf(";");
+           const int NEXT_IND = INDEX + 1;
+           const QString board_mask = m.mid(0, INDEX);
+           const QString moves_history = m.mid(NEXT_IND, m.indexOf(";", NEXT_IND) - NEXT_IND);
+           const QString move_num = m.mid(m.indexOf(";", NEXT_IND) + 1);
+
+           board_graphic.set_board_mask(board_mask);
+           board_graphic.set_move_color(move_num.toInt());
+           board_graphic.set_connect_status(Messages::SERVER_HERE);
+           board_graphic.set_moves_history(moves_history);
+           board_graphic.update_hilight(move_num.toInt(),moves_history);
+
+           if(moves_history.endsWith("#"))
+             { board_graphic.set_check_mate(); }
+         }
+         else qDebug()<<"Warning! in Exporter::push_to_graphic: Unknown message type";
+      }
     }
   }
 
