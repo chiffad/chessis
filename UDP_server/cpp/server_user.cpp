53779d86a06b300ba5d283f834997e73975ffb3f
Dmytro Prokofiev <prokofjev.d@gmail.com>
2016-11-25 20:18:09 +0200

    update serser_user class . Now it can pull and push messages. When it push message, it add serial num

--- UDP_server/cpp/server_user.cpp
+++ UDP_server/cpp/server_user.cpp
@@ -2,17 +2,21 @@
 
 #include <QVector>
 
+
 using namespace sr;
 
 struct server_user::impl_t
 {
-  impl_t(const int port, const QHostAddress& ip);
+  impl_t(const int port, const QHostAddress& address);
   void push(const QByteArray& m);
   QByteArray pull();
   bool is_empty() const;
 
   const int port;
   const QHostAddress& ip;
+  int send_serial_num = 0;
+
+  QVector<QByteArray> messages;
 };
 
 server_user::server_user(const int port, const QHostAddress& ip)
@@ -38,3 +42,31 @@ bool server_user::is_empty() const
 {
   return impl->is_empty();
 }
+
+server_user::impl_t::impl_t(const int p, const QHostAddress& address)
+    : port(), ip(address)
+{
+}
+
+void server_user::impl_t::push(const QByteArray& m)
+{
+  messages.append(m);
+}
+
+QByteArray server_user::impl_t::pull()
+{
+  QByteArray m = QByteArray::number(send_serial_num);
+  m.append(" ");
+  m.append(messages.first());
+
+  messages.removeFirst();
+  ++send_serial_num;
+
+  return m;
+}
+
+bool server_user::impl_t::is_empty() const
+{
+  return messages.isEmpty();
+}
+
