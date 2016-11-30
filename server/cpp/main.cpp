e7bf59c9b81a8043ebe5f4e0565a8443bbfaa566
Dmytro Prokofiev <prokofjev.d@gmail.com>
2016-11-30 13:22:13 +0200

    add exception hendler and logic. Now it sending board state

--- server/cpp/main.cpp
+++ server/cpp/main.cpp
@@ -2,6 +2,7 @@
 #include <QDebug>
 #include <QVector>
 #include <memory>
+#include <exception>
 
 #include "server.h"
 #include "user.h"
@@ -9,7 +10,7 @@
 #include "messages.h"
 
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]) try
 {
   QApplication app(argc, argv);
 
@@ -25,13 +26,17 @@ int main(int argc, char *argv[])
       const auto u = std::find_if(users.begin(), users.end(), [&](const auto& user){ return user->get_ip() == i.ip && user->get_port() == i.port;});
 
       if(u == users.end())
-        { users.append(std::make_shared<sr::user_t>(i.ip, i.port)); }
+        { qDebug()<<"============"; qDebug()<<"create new user in main!"; users.append(std::make_shared<sr::user_t>(i.ip, i.port)); }
     }
 
     for(auto& u : users)
     {
       const auto ip = u->get_ip();
       const auto port = u->get_port();
+
+      if(!server.is_message_append(port, ip))
+        { continue; }
+
       const auto m = server.pull(port, ip);
       const auto type = messages::MESSAGE(m.mid(0, m.indexOf(" ")).toInt());
 
@@ -65,14 +70,19 @@ int main(int argc, char *argv[])
       }
     }
 
-    const auto u1 = std::find_if(users.begin(), users.end(), [](const auto& i){ return i->is_game_active();});
-    const auto u2 = std::find_if(u1, users.end(), [](const auto& i){ return i->is_game_active();});
+    auto u1 = std::find_if(users.begin(), users.end(), [](const auto& i){ return !i->is_game_active();});
+    auto u2 = std::find_if(u1, users.end(), [](const auto& i){ return !i->is_game_active();});
 
     if(u1 != users.end() && u2 != users.end())
     {
+      qDebug()<<"===========";
+      qDebug()<<"!!creating new board";
       std::shared_ptr<logic::desk_t> d(std::make_shared<logic::desk_t>());
       (*u1)->set_board(d);
       (*u2)->set_board(d);
+
+      server.push((*u1)->get_board_state(), (*u1)->get_port(), (*u1)->get_ip());
+      server.push((*u2)->get_board_state(), (*u2)->get_port(), (*u2)->get_ip());
     }
 
     server.process_event();
@@ -80,3 +90,9 @@ int main(int argc, char *argv[])
 
   return 0;
 }
+
+catch(std::exception const& ex)
+{
+  qDebug()<<"Exception!"<<ex.what();
+}
+
