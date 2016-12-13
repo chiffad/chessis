914a11b8c6b7d093ef3ed61387c64bb55c094ec5
Dmytro Prokofiev <prokofjev.d@gmail.com>
2016-12-13 13:16:40 +0200

    update

--- server/cpp/main.cpp
+++ server/cpp/main.cpp
@@ -3,8 +3,10 @@
 #include <QVector>
 #include <memory>
 #include <exception>
+#include <algorithm>
 
 #include "server.h"
+#include "client.h"
 
 
 int main(int argc, char *argv[]) try
@@ -12,6 +14,33 @@ int main(int argc, char *argv[]) try
   QApplication app(argc, argv);
 
   sr::server_t server;
+  QVector<std::shared_ptr<sr::client_t>> clients;
+
+  while(true)
+  {
+    app.processEvents();
+    for(auto data : server.pull())
+    {
+      auto client = std::find_if(clients.begin(), clients.end(), [&data](const auto& i){ return (i->get_port() == data.port && i->get_ip() == data.ip); });
+
+      if(client == clients.end())
+      {
+        clients.append(std::make_shared<sr::client_t>(data.port, data.ip));
+        client = &clients.last();
+      }
+
+      (*client)->push(data.message);
+    }
+
+    for(auto client : clients)
+    {
+      if(client->is_message_for_server_append())
+        { server.send(client->pull_for_server(), client->get_port(), client->get_ip()); }
+
+      if(client->is_message_for_logic_append())
+        { server.send(client->pull_for_server(), client->get_port(), client->get_ip()); }
+    }
+  }
 
   return 0;
 }
