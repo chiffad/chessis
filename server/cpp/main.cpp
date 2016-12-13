b49121a068fca695f0fd78f44e3fbc453ec9e4f1
Dmytro Prokofiev <prokofjev.d@gmail.com>
2016-12-13 18:54:28 +0200

    big update. Start to process logic message

--- server/cpp/main.cpp
+++ server/cpp/main.cpp
@@ -1,4 +1,5 @@
 #include <QApplication>
+#include <QByteArray>
 #include <QDebug>
 #include <QVector>
 #include <memory>
@@ -8,6 +9,8 @@
 #include "server.h"
 #include "client.h"
 #include "desk.h"
+#include "log.h"
+#include "messages.h"
 
 
 int main(int argc, char *argv[]) try
@@ -38,7 +41,7 @@ int main(int argc, char *argv[]) try
         }
       }
 
-      (*c)->push(data.message);
+      (*c)->push_from_server(data.message);
     }
 
     for(auto c : clients)
@@ -51,8 +54,59 @@ int main(int argc, char *argv[]) try
         const auto message = c->pull_for_logic();
         auto desk = std::find_if(desks.begin(), desks.end(), [&c](auto d){ return d->is_contain_player(c); });
 
+        const int type = message.mid(0, message.indexOf(" ")).toInt();
+
         if(desk == desks.end())
-          { qDebug()<<"main: desk == desk.end()"; continue; }
+        {
+          qDebug()<<"main: desk == desk.end()";
+
+          if(type == messages::OPPONENT_INF)
+            { c->push_for_send("No opponent: no game in progress!"); }
+
+          continue;
+        }
+
+        const auto data = message.mid(message.indexOf(" ") + 1);
+        qDebug()<<"main: message!!: "<<data;
+
+        switch(type)
+        {
+          //case messages::MESSAGE_RECEIVED: //in client
+          //case messages::IS_SERVER_LOST: // no need cause on this message already was sended responce
+
+          case messages::OPPONENT_INF:
+            //need update!
+            break;
+          case messages::MY_INF:
+            //need update!
+            break;
+          case messages::MOVE:
+            (*desk)->make_moves_from_str(data.toStdString());
+            break;
+
+          default:
+            switch(type)
+            {
+              case messages::MOVE:
+                (*desk)->make_moves_from_str(data.toStdString());
+                break;
+//need more cases!!
+              default:
+                sr::throw_exception("Uncnown message type!: ", type);
+            }
+            const QByteArray m(QByteArray::number(messages::GAME_INF)
+                        + " "
+                        + QByteArray::fromStdString((*desk)->get_board_mask())
+                        + ";"
+                        + QByteArray::fromStdString((*desk)->get_moves_history())
+                        + ((*desk)->is_mate() ? "#;" : ";")
+                        + QByteArray::number((*desk)->get_move_num()));
+            c->push_for_send(m);
+
+            const auto const_c2 = (*desk)->get_first_player().lock() == c ? (*desk)->get_second_player().lock() : (*desk)->get_first_player().lock();
+            auto c2 = std::find(clients.begin(), clients.end(), const_c2);
+            (*c2)->push_for_send(m);
+        }
       }
     }
   }
@@ -64,4 +118,3 @@ catch(std::exception const& ex)
 {
   qDebug()<<"Exception!"<<ex.what();
 }
-
