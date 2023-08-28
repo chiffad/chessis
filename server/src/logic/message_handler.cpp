0b90e7d475e138faf9b63452487e09cbc27e5adf
Dmytro <prokofjev.d@gmail.com>
2023-08-28 21:59:29 +0300

    [server] use pimpl for message_handler

--- server/src/logic/message_handler.cpp
+++ server/src/logic/message_handler.cpp
@@ -1,9 +1,10 @@
-#include "logic/handle_message.hpp"
+#include "logic/message_handler.hpp"
 
 #include "messages/messages.hpp"
 
-#include "common/helper.hpp"
+#include <boost/mpl/begin_end.hpp>
 #include <spdlog/spdlog.h>
+#include <typeinfo>
 
 namespace logic {
 
@@ -20,27 +21,80 @@ inline msg::inf_request_t get_person_inf(const player_t& player)
 
 } // namespace
 
-message_handler_t::message_handler_t(games_manager_t& games_manager, server::server_t& server)
-  : games_manager_{games_manager}
-  , server_{server}
-{}
-
-void message_handler_t::process_server_message(const endpoint_t& addr, const std::string& message)
+struct message_handler_t::impl_t
 {
-  auto c = games_manager_.player(addr);
-  if (c)
+  impl_t(games_manager_t& games_manager, server::server_t& server)
+    : games_manager_{games_manager}
+    , server_{server}
+  {}
+
+  void process_mess_begin(const std::string& str, player_t& player) { process_mess<boost::mpl::begin<msg::message_types>::type>(str, player); }
+
+  template<typename T>
+  void process_mess(const std::string& str, player_t& player)
   {
-    handle(message, *c.value());
-    return;
+    if (msg::is_equal_types<typename T::type>(str))
+    {
+      handle_fn<typename T::type>(str, player);
+      return;
+    }
+
+    process_mess<typename boost::mpl::next<T>::type>(str, player);
   }
 
-  SPDLOG_INFO("New player! addr={}", addr);
-  const auto uuid = games_manager_.add_player(addr);
-  handle(message, games_manager_.player(uuid));
+  template<msg::one_of_msg_types T>
+  void handle_fn(const std::string& str, player_t& /*player*/)
+  {
+    SPDLOG_ERROR("For type={} tactic isn't defined! msg={}", typeid(T).name(), str);
+  }
+
+  void start_new_game(player_t& player)
+  {
+    const auto free_player_uuid = games_manager_.free_player(player);
+    if (!free_player_uuid)
+    {
+      SPDLOG_INFO("Can not start game for cleint={}; not opponent found!", player);
+      return;
+    }
+
+    games_manager_.start_game(player.uuid(), free_player_uuid.value());
+    board_updated(player);
+  }
+
+  void board_updated(player_t& player)
+  {
+    const auto board_uuid = games_manager_.board(player);
+    if (!board_uuid)
+    {
+      SPDLOG_ERROR("No board found for player={}", player);
+      return;
+    }
+
+    const auto& board = games_manager_.board(board_uuid.value());
+    server_.send(get_board_state(board, player.playing_white()), player.address());
+
+    const auto opp_uuid = games_manager_.opponent(player.uuid());
+    if (!opp_uuid)
+    {
+      SPDLOG_INFO("No opponent found for player={}", player);
+      return;
+    }
+
+    server_.send(get_board_state(board, !player.playing_white()), games_manager_.player(opp_uuid.value()).address());
+  }
+
+  games_manager_t& games_manager_;
+  server::server_t& server_;
+};
+
+template<>
+void message_handler_t::impl_t::process_mess<boost::mpl::end<msg::message_types>::type>(const std::string& str, player_t& player)
+{
+  SPDLOG_ERROR("No type found for str={}; player={};", str, player);
 }
 
 template<>
-void message_handler_t::handle_fn<msg::login_t>(const std::string& message, player_t& player)
+void message_handler_t::impl_t::handle_fn<msg::login_t>(const std::string& message, player_t& player)
 {
   SPDLOG_DEBUG("tactic for login_t; msg={}", message);
   auto login = msg::init<msg::login_t>(message);
@@ -58,7 +112,7 @@ void message_handler_t::handle_fn<msg::login_t>(const std::string& message, play
 }
 
 template<>
-void message_handler_t::handle_fn<msg::opponent_inf_t>(const std::string& message, player_t& player)
+void message_handler_t::impl_t::handle_fn<msg::opponent_inf_t>(const std::string& message, player_t& player)
 {
   SPDLOG_DEBUG("tactic for opponent_inf_t; msg={}", message);
   const auto opp_uuid = games_manager_.opponent(player.uuid());
@@ -72,14 +126,14 @@ void message_handler_t::handle_fn<msg::opponent_inf_t>(const std::string& messag
 }
 
 template<>
-void message_handler_t::handle_fn<msg::my_inf_t>(const std::string& message, player_t& player)
+void message_handler_t::impl_t::handle_fn<msg::my_inf_t>(const std::string& message, player_t& player)
 {
   SPDLOG_DEBUG("tactic for my_inf_t; msg={}", message);
   server_.send(get_person_inf(player), player.address());
 }
 
 template<>
-void message_handler_t::handle_fn<msg::client_lost_t>(const std::string& message, player_t& player)
+void message_handler_t::impl_t::handle_fn<msg::client_lost_t>(const std::string& message, player_t& player)
 {
   SPDLOG_DEBUG("tactic for client_lost_t; msg={}", message);
 
@@ -91,7 +145,7 @@ void message_handler_t::handle_fn<msg::client_lost_t>(const std::string& message
 }
 
 template<>
-void message_handler_t::handle_fn<msg::move_t>(const std::string& message, player_t& player)
+void message_handler_t::impl_t::handle_fn<msg::move_t>(const std::string& message, player_t& player)
 {
   SPDLOG_DEBUG("tactic for move_t; msg={}", message);
 
@@ -107,7 +161,7 @@ void message_handler_t::handle_fn<msg::move_t>(const std::string& message, playe
 }
 
 template<>
-void message_handler_t::handle_fn<msg::back_move_t>(const std::string& message, player_t& player)
+void message_handler_t::impl_t::handle_fn<msg::back_move_t>(const std::string& message, player_t& player)
 {
   SPDLOG_DEBUG("tactic for back_move_t; msg={}", message);
 
@@ -123,7 +177,7 @@ void message_handler_t::handle_fn<msg::back_move_t>(const std::string& message, 
 }
 
 template<>
-void message_handler_t::handle_fn<msg::go_to_history_t>(const std::string& message, player_t& player)
+void message_handler_t::impl_t::handle_fn<msg::go_to_history_t>(const std::string& message, player_t& player)
 {
   SPDLOG_DEBUG("tactic for go_to_history_t; msg={}", message);
 
@@ -139,51 +193,31 @@ void message_handler_t::handle_fn<msg::go_to_history_t>(const std::string& messa
 }
 
 template<>
-void message_handler_t::handle_fn<msg::new_game_t>(const std::string& message, player_t& player)
+void message_handler_t::impl_t::handle_fn<msg::new_game_t>(const std::string& message, player_t& player)
 {
   SPDLOG_DEBUG("tactic for start_new_game_t; msg={}", message);
   start_new_game(player);
 }
 
-template<>
-void message_handler_t::process_mess<boost::mpl::end<msg::message_types>::type>(const std::string& /*str*/, player_t& /*player*/)
-{
-  SPDLOG_ERROR("no type found");
-}
+message_handler_t::message_handler_t(games_manager_t& games_manager, server::server_t& server)
+  : impl_(std::make_unique<impl_t>(games_manager, server))
+{}
 
-void message_handler_t::start_new_game(player_t& player)
+message_handler_t::~message_handler_t() = default;
+
+void message_handler_t::process_server_message(const endpoint_t& addr, const std::string& message)
 {
-  const auto free_player_uuid = games_manager_.free_player(player);
-  if (!free_player_uuid)
+  auto c = impl_->games_manager_.player(addr);
+  // TODO: simplify!
+  if (c)
   {
-    SPDLOG_INFO("Can not start game for cleint={}; not opponent found!", player);
+    impl_->process_mess_begin(message, *c.value());
     return;
   }
 
-  games_manager_.start_game(player.uuid(), free_player_uuid.value());
-  board_updated(player);
+  SPDLOG_INFO("New player! addr={}", addr);
+  const auto uuid = impl_->games_manager_.add_player(addr);
+  impl_->process_mess_begin(message, impl_->games_manager_.player(uuid));
 }
 
-void message_handler_t::board_updated(player_t& player)
-{
-  const auto board_uuid = games_manager_.board(player);
-  if (!board_uuid)
-  {
-    SPDLOG_ERROR("No board found for player={}", player);
-    return;
-  }
-
-  const auto& board = games_manager_.board(board_uuid.value());
-  server_.send(get_board_state(board, player.playing_white()), player.address());
-
-  const auto opp_uuid = games_manager_.opponent(player.uuid());
-  if (!opp_uuid)
-  {
-    SPDLOG_INFO("No opponent found for player={}", player);
-    return;
-  }
-
-  server_.send(get_board_state(board, !player.playing_white()), games_manager_.player(opp_uuid.value()).address());
-}
-
-} // namespace server
+} // namespace logic
--- server/src/logic/message_handler.hpp
+++ server/src/logic/message_handler.hpp
@@ -2,74 +2,27 @@
 
 #include "common/helper.hpp"
 #include "logic/games_manager.hpp"
-#include "messages/messages.hpp"
 #include "server/server.hpp"
-
-#include <spdlog/spdlog.h>
-
-#include <boost/mpl/begin_end.hpp>
 #include <memory>
 #include <string>
-#include <typeinfo>
 
 namespace logic {
+
 class message_handler_t
 {
 public:
-  explicit message_handler_t(games_manager_t&, server::server_t&);
+  message_handler_t(games_manager_t&, server::server_t&);
+  message_handler_t(const message_handler_t&) = delete;
+  message_handler_t(message_handler_t&&) = default;
+  message_handler_t& operator=(const message_handler_t&) = delete;
+  message_handler_t& operator=(message_handler_t&&) = default;
+  ~message_handler_t();
 
   void process_server_message(const endpoint_t& addr, const std::string& message);
 
-  // TODO: not needed as public
-#define handle(str, player) process_mess<boost::mpl::begin<msg::message_types>::type>(str, player);
-
-  template<typename T>
-  void process_mess(const std::string& str, player_t& player)
-  {
-    if (msg::is_equal_types<typename T::type>(str))
-    {
-      handle_fn<typename T::type>(str, player);
-    }
-    else
-    {
-      process_mess<typename boost::mpl::next<T>::type>(str, player);
-    }
-  }
-
-public:
-  message_handler_t(const message_handler_t&) = delete;
-  message_handler_t& operator=(const message_handler_t&) = delete;
-  message_handler_t(message_handler_t&&) = default;
-  message_handler_t& operator=(message_handler_t&&) = default;
-
 private:
-  void board_updated(player_t& player);
-  void start_new_game(player_t& player);
-
-  template<typename T>
-  void handle_fn(const std::string& str, player_t& /*player*/)
-  {
-    SPDLOG_ERROR("For type={} tactic isn't defined! msg={}", typeid(T).name(), str);
-  }
-
-  games_manager_t& games_manager_;
-  server::server_t& server_;
+  struct impl_t;
+  std::unique_ptr<impl_t> impl_;
 };
 
-template<>
-void message_handler_t::process_mess<boost::mpl::end<msg::message_types>::type>(const std::string& /*str*/, player_t& /*player*/);
-
-#define handle_macro(struct_type)                                                                                                                              \
-  template<>                                                                                                                                                   \
-  void message_handler_t::handle_fn<struct_type>(const std::string& str, player_t& player);
-handle_macro(msg::login_t);
-handle_macro(msg::opponent_inf_t);
-handle_macro(msg::my_inf_t);
-handle_macro(msg::client_lost_t);
-handle_macro(msg::move_t);
-handle_macro(msg::back_move_t);
-handle_macro(msg::go_to_history_t);
-handle_macro(msg::new_game_t);
-#undef handle_macro
-
-} // namespace server
+} // namespace logic
